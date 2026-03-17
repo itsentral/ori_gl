1d1108a02811c5072f485528c7d4c6dd367b5578
SamSyamsudin <syams.69@gmail.com>
2026-03-17 16:16:05 +0700

    Update M_pdf.php

--- application/libraries/M_pdf.php
+++ application/libraries/M_pdf.php
@@ -1,42 +1,43 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
 class M_pdf
 {
-
-    function m_pdf()
+    function __construct()
     {
         $CI = &get_instance();
-        log_message('Debug', 'mPDF class is loaded.');
+        log_message('debug', 'mPDF class is loaded.');
     }
 
     function load($param = NULL)
     {
         include_once APPPATH . 'libraries/MPDF57/mpdf.php';
 
-        if ($params == NULL) {
-            $param = '"en-GB-x","A5","","",10,10,10,10,6,3';
+        if ($param == NULL) {
+            return new mPDF('en-GB-x', 'A5', '', '', 10, 10, 10, 10, 6, 3);
         }
 
         return new mPDF($param);
     }
+
     function load_a4($param = NULL)
     {
         include_once APPPATH . 'libraries/MPDF57/mpdf.php';
 
-        if ($params == NULL) {
-            $param = '"en-GB-x","A5","","",10,10,10,10,6,3';
+        if ($param == NULL) {
+            return new mPDF('en-GB-x', 'A4', '', '', 10, 10, 10, 10, 6, 3);
         }
 
         return new mPDF($param);
     }
-    function load_c($param = NULL) //pake yang load_c
+
+    function load_c($param = NULL)
     {
         include_once APPPATH . 'libraries/MPDF57/mpdf.php';
 
-        if ($params == NULL) {
-            $param = '"A5"';
-            // $param = '"en-GB-x","A5","","",0,0,0,0,0,0';
+        if ($param == NULL) {
+            return new mPDF('en-GB-x', 'A5');
         }
 
-        return new mPDF($param); //jadi pas didsini di ganti pas di print pprofiew di pdf jadinya landscape
+        return new mPDF($param);
     }
 }
